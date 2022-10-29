ecfde565ca497fe87d866e4cb4b120ecd920603e
Paweł Stosio <pawel.stosio@cdprojektred.com>
2022-10-29 18:45:30 +0200

    added bigger icons for win and linux

--- src/handlers/handleGetIcon.tsx
+++ src/handlers/handleGetIcon.tsx
@@ -2,7 +2,10 @@ import os from 'os';
 import { app, IpcMainInvokeEvent } from 'electron';
 
 export async function handleGetIcon(event: IpcMainInvokeEvent, path: string) {
-  const image = await app.getFileIcon(`${os.homedir()}/${path}`, { size: 'normal' });
-  
+  const image = await app.getFileIcon(`${os.homedir()}/${path}`, {
+    // https://www.electronjs.org/docs/latest/api/app#appgetfileiconpath-options
+    size: os.platform() === 'win32' || os.platform() === 'linux' ? 'large' : 'normal',
+  });
+
   return image.toDataURL();
 }
